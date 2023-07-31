c95b6edcab16179959bd2a7d4daa123cd066d57d
--global <aleksey.cherkas99@gmail.com>
2023-07-31 16:22:39 +0300

    init: start reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+  let numbers = n.toString().split("");
+  let joinNumbers = [];
+
+  for(let i = 0; i < numbers.length; i++) {
+    joinNumbers.unshift(numbers[i]);
+  }
+
+  return parseInt(joinNumbers.join(""));
 }
